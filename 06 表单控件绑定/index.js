f7d7af25705d0b3c9301c49e62bf99ee5925ded7
Yangxg <jessebutchman@gmail.com>
2016-10-03 21:53:01 +0800

    add

--- 06 表单控件绑定/index.js
+++ 06 表单控件绑定/index.js
@@ -33,3 +33,37 @@ new Vue({
 		multiple_selected:[]
 	}
 })
+
+new Vue({
+	el:"#vmodel6",
+	data:{
+		selected: 'B',
+		options:[
+			{value:"A",text:"OA"},
+			{value:"B",text:"OB"},
+			{value:"C",text:"OC"},
+		]
+	}
+})
+
+new Vue({
+	el:"#vmodel7",
+	data:{
+		toggle:"false22",
+		a:"true11",
+		b:"false22",
+		pick:"radio_b",
+		r_a:"radio_a",
+		r_b:"radio_b",
+		s_selected:{number:456}
+	}
+})
+
+var vm8 = new Vue({
+	el:"#vmodel8",
+	data:{
+		msg:"init value",
+		debounce_msg:"init value",
+		number_msg:222
+	}
+})
